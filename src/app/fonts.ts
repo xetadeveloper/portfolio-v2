961d4b2ba1af0f7f622f934b2fd5eb633b448ce4
xetadeveloper <jazelogin@hotmail.com>
2024-05-21 22:59:51 +0100

    feat: setup chakra UI in project

---
+++ src/app/fonts.ts
@@ -0,0 +1,11 @@
+import { Hind_Siliguri, Inter } from 'next/font/google';
+
+const hindSiliguri = Hind_Siliguri({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['400'],
+});
+
+export const fonts = {
+    hindSiliguri,
+};
