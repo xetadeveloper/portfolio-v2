b58f07cd685a2043e04f0c3249feb96caee92120
xetadeveloper <jazelogin@hotmail.com>
2024-06-20 16:46:54 +0100

    feat: use v10 for generating contentful types

--- src/contentful/types/HomeType.ts
+++
@@ -1,7 +0,0 @@
-import { EntrySkeletonType } from 'contentful';
-import { TypeHomeFields } from '../__generated__';
-
-export interface HomeType extends EntrySkeletonType {
-    contentTypeId: string;
-    fields: TypeHomeFields;
-}
--- src/contentful/types/ProjectType.ts
+++
@@ -1,7 +0,0 @@
-import { EntrySkeletonType } from 'contentful';
-import { TypeProjectFields } from '../__generated__';
-
-export interface ProjectType extends EntrySkeletonType {
-    contentTypeId: string;
-    fields: TypeProjectFields;
-}
