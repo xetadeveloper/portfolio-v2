6577a20ba56fa20266b5027df5a8f5adefdea0eb
xetadeveloper <jazelogin@hotmail.com>
2024-06-23 15:20:09 +0100

    feat: use correct icons for tech stack

--- src/utils/techStackIcons.tsx
+++ src/utils/techStackIcons.tsx
@@ -1,8 +1,21 @@
 import { CSSProperties } from 'react';
+import { BsGitlab } from 'react-icons/bs';
 import { FaQuestionCircle } from 'react-icons/fa';
-import { FaGit, FaGithub, FaNodeJs, FaReact } from 'react-icons/fa6';
-import { RiJavascriptFill, RiNextjsLine } from 'react-icons/ri';
-import { SiExpress, SiTypescript } from 'react-icons/si';
+import { FaDocker, FaGit, FaGithub, FaNode, FaPython, FaReact } from 'react-icons/fa6';
+import { IoLogoPwa } from 'react-icons/io5';
+import { RiJavascriptFill, RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri';
+import {
+    SiChakraui,
+    SiDjango,
+    SiExpress,
+    SiFramer,
+    SiJekyll,
+    SiMongodb,
+    SiMui,
+    SiMysql,
+    SiPostgresql,
+    SiTypescript,
+} from 'react-icons/si';
 
 type stackName = 'react' | 'nodejs' | 'typescript' | 'github' | 'git' | 'expressjs' | 'javascript';
 
@@ -11,7 +24,7 @@ export function getTechIcon({ icon, styles }: { icon: stackName | string; styles
         case 'react':
             return <FaReact style={{ height: '100%', width: '100%', ...styles }} />;
         case 'nodejs':
-            return <FaNodeJs style={{ height: '100%', width: '100%', ...styles }} />;
+            return <FaNode style={{ height: '100%', width: '100%', ...styles }} />;
         case 'typescript':
             return <SiTypescript style={{ height: '100%', width: '100%', ...styles }} />;
         case 'github':
@@ -19,14 +32,42 @@ export function getTechIcon({ icon, styles }: { icon: stackName | string; styles
         case 'git':
             return <FaGit style={{ height: '100%', width: '100%', ...styles }} />;
         case 'nextjs':
-            return <RiNextjsLine style={{ height: '100%', width: '100%', ...styles }} />;
+            return <RiNextjsFill style={{ height: '100%', width: '100%', ...styles }} />;
         case 'expressjs':
-            return <SiExpress style={{ height: '100%', width: '100%' }} />;
+            return <SiExpress style={{ height: '100%', width: '100%', ...styles }} />;
         case 'javascript':
-            return <RiJavascriptFill style={{ height: '100%', width: '100%' }} />;
+            return <RiJavascriptFill style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'mongodb':
+            return <SiMongodb style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'chakraui':
+            return <SiChakraui style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'django':
+            return <SiDjango style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'tailwindcss':
+            return <RiTailwindCssFill style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'postgresdb':
+            return <SiPostgresql style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'pwa':
+            return <IoLogoPwa style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'mysql':
+            return <SiMysql style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'framermotion':
+            return <SiFramer style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'gitlab':
+            return <BsGitlab style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'python':
+            return <FaPython style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'jekyll':
+            return <SiJekyll style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'websocket':
+            return <SiMongodb style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'docker':
+            return <FaDocker style={{ height: '100%', width: '100%', ...styles }} />;
+        case 'materialui':
+            return <SiMui style={{ height: '100%', width: '100%', ...styles }} />;
     }
 
-    return <FaQuestionCircle style={{ height: '100%', width: '100%' }} />;
+    return <FaQuestionCircle style={{ height: '100%', width: '100%', ...styles }} />;
 }
 
 export function getTechDisplayTitle(stack: string) {
@@ -47,6 +88,34 @@ export function getTechDisplayTitle(stack: string) {
             return 'ExpressJS';
         case 'javascript':
             return 'Javascript';
+        case 'mongodb':
+            return 'MongoDB';
+        case 'django':
+            return 'Django';
+        case 'tailwindcss':
+            return 'Tailwind CSS';
+        case 'chakraui':
+            return 'Chakra UI';
+        case 'materialui':
+            return 'Material UI';
+        case 'postgresdb':
+            return 'PostgresDB';
+        case 'pwa':
+            return 'Progressive Web Apps';
+        case 'mysql':
+            return 'MySQL';
+        case 'framermotion':
+            return 'Framer Motion';
+        case 'gitlab':
+            return 'GitLab';
+        case 'python':
+            return 'Python';
+        case 'jekyll':
+            return 'Jekyll';
+        case 'websocket':
+            return 'WebSockets';
+        case 'docker':
+            return 'Docker';
     }
 
     return stack;
