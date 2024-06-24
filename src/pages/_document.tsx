648714564c2bf19a49b34c0e8ed9c1ab910b39bb
xetadeveloper <jazelogin@hotmail.com>
2024-06-24 16:52:27 +0100

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,16 @@ import { fonts } from '~/assets/fonts';
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link rel="icon" href="/favicon/favicon.ico" type="image/ico" />
+                <link rel="icon" sizes="512x512" href="/favicon/android-chrome-512x512.png" />
+                <link rel="icon" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                <link rel="manifest" href="/favicon/site.webmanifest" />
+            </Head>
+
             <body className={[fonts.hindSiliguri.className].join(' ')}>
                 <Main />
                 <NextScript />
