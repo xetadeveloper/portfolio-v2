907a0a62a390ca47d77a95011c32184b146c41bf
xetadeveloper <jazelogin@hotmail.com>
2025-06-22 09:14:23 -0400

    feat: add vercel analytics to site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,12 @@ import type { AppProps } from 'next/app';
 import { theme } from '~/theme';
 import MainLayout from '~/layouts/MainLayout';
 import '../css/globals.css';
-
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps, router }: AppProps) {
     return (
         <ChakraProvider theme={theme} resetCSS={true}>
+            <Analytics />
             <AnimatePresence mode="wait">
                 <MainLayout key={router.route}>
                     <Component key={router.route} {...pageProps} />
